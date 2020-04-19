f7846ad4c24118a84a7f99b1a31bcba450c84f1e
suzuryo <suzuryo@gmail.com>
2020-04-19 09:12:40 +0900

    fix: ConfirmedCasesNuimberCardの更新日は patients_summary.date を参照する
    東京版のBUGでは？

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -5,7 +5,7 @@
       :title-id="'number-of-confirmed-cases'"
       :chart-id="'time-bar-chart-patients'"
       :chart-data="patientsGraph"
-      :date="Data.patients.date"
+      :date="Data.patients_summary.date"
       :unit="$t('人')"
     />
   </v-col>
