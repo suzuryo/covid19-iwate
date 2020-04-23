b71ae813c077e64a8d09cdc097ac302ef94502e7
suzuryo <suzuryo@gmail.com>
2020-04-23 18:24:11 +0900

    オープンデータへのリンクを削除

--- components/cards/ConfirmedCasesNumberCard.vue
+++ components/cards/ConfirmedCasesNumberCard.vue
@@ -7,7 +7,6 @@
       :chart-data="patientsGraph"
       :date="Data.patients_summary.date"
       :unit="$t('人')"
-      :url="'https://catalog.data.metro.tokyo.lg.jp/dataset/t000010d0000000068'"
     >
     </time-bar-chart>
   </v-col>
