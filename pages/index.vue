169d84906034c0bc1d971340b15d9caf60aeb596
suzuryo <suzuryo@gmail.com>
2020-07-05 16:01:47 +0900

    remove: disable ConfirmedCaseAttributesCard

--- pages/index.vue
+++ pages/index.vue
@@ -63,7 +63,7 @@ import Data from '@/data/data.json'
 import News from '@/data/news.json'
 import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsCard.vue'
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
-import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
+// import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
 // import ConfirmedCasesByMunicipalitiesCard from '@/components/cards/ConfirmedCasesByMunicipalitiesCard.vue'
 // import TestedCasesDetailsCard from '@/components/cards/TestedCasesDetailsCard.vue'
 // import InspectionPersonsNumberCard from '@/components/cards/InspectionPersonsNumberCard.vue'
@@ -83,7 +83,7 @@ export default Vue.extend({
     CardRow,
     ConfirmedCasesDetailsCard,
     ConfirmedCasesNumberCard,
-    ConfirmedCasesAttributesCard,
+    // ConfirmedCasesAttributesCard,
     // ConfirmedCasesByMunicipalitiesCard,
     // TestedCasesDetailsCard,
     // InspectionPersonsNumberCard,
