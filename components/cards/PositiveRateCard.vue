2a0c8d37e6607e35e66ff46925b2912de412bbfb
suzuryo <suzuryo@gmail.com>
2020-09-05 00:47:49 +0900

    検査の陽性率・PCR検査の7日間移動平均に岩手県の検査可能件数を掲載

--- components/cards/PositiveRateCard.vue
+++ components/cards/PositiveRateCard.vue
@@ -15,59 +15,59 @@
         :table-labels="positiveRateTableLabels"
       >
         <template v-slot:additionalDescription>
-          <span>{{ $t('（注）') }}</span>
-          <ul>
-            <li>
-              {{
-                $t(
-                  '陽性率：陽性判明数（PCR・抗原）の移動平均／検査件数（＝陽性判明数（PCR・抗原）＋陰性判明数（PCR・抗原））の移動平均'
-                )
-              }}
-            </li>
-            <li>
-              {{
-                $t(
-                  '集団感染発生や曜日による数値のばらつきにより、日々の結果が変動するため、こうしたばらつきを平準化し全体の傾向を見る趣旨から、過去7日間の移動平均値をもとに算出し、折れ線グラフで示す（例えば、8月7日の陽性率は、8月1日から8月7日までの実績平均を用いて算出）'
-                )
-              }}
-            </li>
-            <li>
-              {{ $t('検査結果の判明日を基準とする') }}
-            </li>
-            <!--
-            <li>
-              {{
-                $t(
-                  '5月7日以降は(1)東京都健康安全研究センター、(2)PCRセンター（地域外来・検査センター）、(3)医療機関での保険適用検査実績により算出。4月10日～5月6日は(3)が含まれず(1)(2)のみ、4月9日以前は(2)(3)が含まれず(1)のみのデータ'
-                )
-              }}
-            </li>
-            <li>
-              {{
-                $t(
-                  '5月13日から6月16日までに行われた抗原検査については、結果が陰性の場合、PCR検査での確定検査が必要であったため、検査件数の二重計上を避けるため、陽性判明数のみ計上。6月17日以降に行われた抗原検査については、陽性判明数、陰性判明数の両方を計上'
-                )
-              }}
-            </li>
-            <li>
-              {{ $t('陰性確認のために行った検査の実施人数は含まない') }}
-            </li>
-            <li>
-              {{
-                $t(
-                  '陽性者が1月24日、25日、30日、2月13日にそれぞれ1名、2月14日に2名発生しているが、有意な数値がとれる2月15日から作成'
-                )
-              }}
-            </li>
-            -->
-            <li>
-              {{
-                $t(
-                  '速報値として公表するものであり、後日確定データとして修正される場合がある'
-                )
-              }}
-            </li>
-          </ul>
+          <div :class="$style.newScenario">
+            <span>{{ $t('（注）') }}</span>
+            <ul>
+              <li>
+                {{
+                  $t(
+                    '陽性率：陽性判明数（PCR・抗原）の移動平均／検査件数（＝陽性判明数（PCR・抗原）＋陰性判明数（PCR・抗原））の移動平均'
+                  )
+                }}
+              </li>
+              <li>
+                {{
+                  $t(
+                    '集団感染発生や曜日による数値のばらつきにより、日々の結果が変動するため、こうしたばらつきを平準化し全体の傾向を見る趣旨から、過去7日間の移動平均値をもとに算出し、折れ線グラフで示す（例えば、8月7日の陽性率は、8月1日から8月7日までの実績平均を用いて算出）'
+                  )
+                }}
+              </li>
+              <li>
+                {{ $t('検査結果の判明日を基準とする') }}
+              </li>
+              <li>
+                {{
+                  $t(
+                    '速報値として公表するものであり、後日確定データとして修正される場合がある'
+                  )
+                }}
+              </li>
+            </ul>
+            <hr />
+            <div :class="$style.newScenarioSummarry">
+              <p>
+                <strong>
+                  {{ $t('岩手県の1日あたりの検査可能件数は864件') }}
+                </strong>
+              </p>
+              <p>
+                {{
+                  $t(
+                    '感染ピーク時の1日あたりの検査件数は、陽性率 10.4% と想定すると、実効再生産数 R1.7 の場合は 444 件、実効再生産数 R2.0 の場合は 829件 として検査態勢を整備している。'
+                  )
+                }}
+              </p>
+            </div>
+            <div :class="$style.newScenarioExternalLink">
+              <div>
+                <external-link
+                  url="https://www.pref.iwate.jp/_res/projects/default_project/_page_/001/028/231/20200710_03.pdf#page=5"
+                >
+                  {{ $t('岩手県 / 第17回本部員会議資料【資料2】[PDF]') }}
+                </external-link>
+              </div>
+            </div>
+          </div>
         </template>
       </positive-rate-mixed-chart>
     </client-only>
@@ -84,11 +84,13 @@ import {
   getNumberToLocaleStringFunction,
   getCommaSeparatedNumberToFixedFunction,
 } from '@/utils/monitoringStatusValueFormatters'
+import ExternalLink from '@/components/ExternalLink'
 dayjs.extend(duration)
 
 export default {
   components: {
     PositiveRateMixedChart,
+    ExternalLink,
   },
   data() {
     // 検査実施日別状況
@@ -143,3 +145,23 @@ export default {
   },
 }
 </script>
+
+<style lang="scss" module>
+.newScenario {
+  hr {
+    border-top: none;
+    border-right: none;
+    border-bottom: 1px solid $gray-4;
+    border-left: none;
+    margin-top: 20px;
+    margin-bottom: 20px;
+  }
+  .newScenarioExternalLink {
+    margin-bottom: 10px;
+    text-align: right;
+  }
+  .newScenarioSummarry {
+    color: red;
+  }
+}
+</style>
