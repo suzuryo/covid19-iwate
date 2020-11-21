2f8c0e9980c37a6d3c98dcd352628e13f4f242b8
suzuryo <suzuryo@gmail.com>
2020-11-21 17:26:28 +0900

    重症が発表されたので、重症項目を設置する

--- components/ConfirmedCasesDetailsTable.vue
+++ components/ConfirmedCasesDetailsTable.vue
@@ -9,7 +9,7 @@
         </span>
       </div>
       <ul :class="$style.group">
-        <li :class="[$style.box]">
+        <li :class="[$style.box, $style.parent]">
           <div :class="$style.content">
             <span>{{ $t('入院') }}</span>
             <span>
@@ -17,23 +17,7 @@
               <span :class="$style.unit">{{ $t('人') }}</span>
             </span>
           </div>
-          <!--
           <ul :class="$style.group">
-            <li :class="[$style.box]">
-              <div :class="$style.content">
-          -->
-          <!-- eslint-disable vue/no-v-html-->
-          <!--
-                <span v-html="$t('軽症・中等症')" />
-          -->
-          <!-- eslint-enable vue/no-v-html-->
-          <!--
-                <span>
-                  <strong>{{ 軽症中等症.toLocaleString() }}</strong>
-                  <span :class="$style.unit">{{ $t('人') }}</span>
-                </span>
-              </div>
-            </li>
             <li :class="[$style.box]">
               <div :class="$style.content">
                 <span>{{ $t('重症') }}</span>
@@ -43,19 +27,7 @@
                 </span>
               </div>
             </li>
-            <li :class="[$style.box, $style.short, $style.severe]">
-              <div :class="$style.pillar">
-                <div :class="$style.content">
-                  <span>{{ $t('不明') }}</span>
-                  <span>
-                    <strong>{{ 不明.toLocaleString() }}</strong>
-                    <span :class="$style.unit">{{ $t('人') }}</span>
-                  </span>
-                </div>
-              </div>
-            </li>
           </ul>
-          -->
         </li>
         <li :class="[$style.box]">
           <div :class="$style.content">
@@ -142,6 +114,10 @@ export default Vue.extend({
       required: true,
     },
     入院療養等調整中: {
+      type: Number,
+      required: true,
+    },
+    重症: {
       type: Number,
       required: true,
     },
