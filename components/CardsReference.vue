e3da4664895022de23835baf515e86f93650a62a
suzuryo <suzuryo@gmail.com>
2020-12-13 21:35:24 +0900

    利用していない項目の並び順を一番下にする

--- components/CardsReference.vue
+++ components/CardsReference.vue
@@ -13,22 +13,22 @@
       <positive-rate-card />
       <!-- 入院患者数 -->
       <hospitalized-number-card />
-      <!-- モニタリング項目(7)重症患者数 -->
-      <!-- <severe-case-card /> -->
       <!-- 陽性者の属性 -->
       <confirmed-cases-attributes-card />
       <!-- 市町村別患者数 -->
       <confirmed-cases-by-municipalities-card />
-      <!-- 発症日別による陽性者数の推移 -->
-      <!-- <positive-number-by-developed-date-card /> -->
-      <!-- 確定日別による陽性者数の推移 -->
-      <!-- <positive-number-by-diagnosed-date-card /> --><!-- 1日あたりの感染者が増えたら機能するが現在はあまり意味のない数字なので非表示 -->
       <!-- 検査実施件数 -->
       <tested-number-card />
       <!-- 受診・相談センター 受付件数 -->
       <monitoring-consultation-desk-reports-number-card />
       <!-- 一般相談 受付件数 -->
       <telephone-advisory-reports-number-card />
+      <!-- 重症患者数 -->
+      <!-- <severe-case-card /> -->
+      <!-- 発症日別による陽性者数の推移 -->
+      <!-- <positive-number-by-developed-date-card /> -->
+      <!-- 確定日別による陽性者数の推移 -->
+      <!-- <positive-number-by-diagnosed-date-card /> --><!-- 1日あたりの感染者が増えたら機能するが現在はあまり意味のない数字なので非表示 -->
     </card-row>
   </div>
 </template>
@@ -40,23 +40,22 @@ import ConfirmedCasesDetailsCard from '@/components/cards/ConfirmedCasesDetailsC
 import ConfirmedCasesNumberCard from '@/components/cards/ConfirmedCasesNumberCard.vue'
 import MonitoringConfirmedCasesNumberCard from '@/components/cards/MonitoringConfirmedCasesNumberCard.vue'
 import PositiveRateCard from '@/components/cards/PositiveRateCard.vue'
-// import SevereCaseCard from '@/components/cards/SevereCaseCard.vue'
 import UntrackedRateCard from '@/components/cards/UntrackedRateCard.vue'
 import HospitalizedNumberCard from '@/components/cards/HospitalizedNumberCard.vue'
 import ConfirmedCasesAttributesCard from '@/components/cards/ConfirmedCasesAttributesCard.vue'
 import ConfirmedCasesByMunicipalitiesCard from '@/components/cards/ConfirmedCasesByMunicipalitiesCard.vue'
 import TestedNumberCard from '@/components/cards/TestedNumberCard.vue'
 import TelephoneAdvisoryReportsNumberCard from '@/components/cards/TelephoneAdvisoryReportsNumberCard.vue'
-// import PositiveNumberByDiagnosedDateCard from '@/components/cards/PositiveNumberByDiagnosedDateCard.vue'
 import MonitoringConsultationDeskReportsNumberCard from '@/components/cards/MonitoringConsultationDeskReportsNumberCard.vue'
+// import SevereCaseCard from '@/components/cards/SevereCaseCard.vue'
 // import PositiveNumberByDevelopedDateCard from '@/components/cards/PositiveNumberByDevelopedDateCard.vue'
+// import PositiveNumberByDiagnosedDateCard from '@/components/cards/PositiveNumberByDiagnosedDateCard.vue'
 
 export default Vue.extend({
   components: {
     CardRow,
     MonitoringConfirmedCasesNumberCard,
     UntrackedRateCard,
-    // SevereCaseCard,
     PositiveRateCard,
     ConfirmedCasesDetailsCard,
     ConfirmedCasesNumberCard,
@@ -66,8 +65,9 @@ export default Vue.extend({
     TestedNumberCard,
     MonitoringConsultationDeskReportsNumberCard,
     TelephoneAdvisoryReportsNumberCard,
-    // PositiveNumberByDiagnosedDateCard,//
+    // SevereCaseCard,
     // PositiveNumberByDevelopedDateCard,//
+    // PositiveNumberByDiagnosedDateCard,//
   },
 })
 </script>
