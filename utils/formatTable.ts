d4e5341d1405e745ada06429fb6379edbe3d3c4a
suzuryo <suzuryo@gmail.com>
2020-12-14 20:09:47 +0900

    発症日の計算BUGを修正

--- utils/formatTable.ts
+++ utils/formatTable.ts
@@ -58,7 +58,7 @@ export default function (data: DataType[]): TableDateType {
         } else if (d['確定日'] === null || d['発症日'] == null) {
           return '不明'
         } else {
-          return `${dayjs(d['確定日']).diff(d['発症日'], 'day')}日前`
+          return `${dayjs(d['陽性確定日']).diff(d['発症日'], 'day')}日前`
         }
       }
       return {
