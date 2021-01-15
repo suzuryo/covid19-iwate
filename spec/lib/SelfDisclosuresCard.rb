21b7131607f270c1427fcbe9044c8c8c879d4e0e
suzuryo <suzuryo@gmail.com>
2021-01-16 01:27:32 +0900

    SelfDisclosuresCard

---
+++ spec/lib/SelfDisclosuresCard.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+def has_self_disclosures_card
+  # h3
+  expect(find('#SelfDisclosuresCard > div > div > div.DataView-Header > div > div:nth-child(1) > h3').text).to eq JA_JSON['SelfDisclosures']['a'][0].to_s
+
+  # テーブルの中身
+  SELF_DISCLOSURES_ITEMS.each_with_index do |d, index|
+    # テーブルの上からi行目をチェックする(icon + text)
+    if d['url']['ja'].blank?
+      expect(page).not_to have_selector("#SelfDisclosuresCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1) > a")
+      expect(find("#SelfDisclosuresCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1)").text).to eq "#{d['text']['ja']}"
+    else
+      expect(find("#SelfDisclosuresCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1) > a").text).to eq "#{d['icon']} #{d['text']['ja']}"
+    end
+
+    # テーブルの上からi行目をチェックする(日付)
+    expect(find("#SelfDisclosuresCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(2)").text).to eq "#{Date.parse(d['date']).strftime("%-m月%-d日")}"
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -34,5 +34,7 @@ NEWS_JSON = JSON.parse(File.read(File.join(__dir__, '../data/news.json')))
 PATIENT_MUNICIPALITIES_JSON = JSON.parse(File.read(File.join(__dir__, '../data/patient_municipalities.json')))
 POSITIVE_RATE_JSON = JSON.parse(File.read(File.join(__dir__, '../data/positive_rate.json')))
 POSITIVE_STATUS_JSON = JSON.parse(File.read(File.join(__dir__, '../data/positive_status.json')))
+SELF_DISCLOSURES_JSON = JSON.parse(File.read(File.join(__dir__, '../data/self_disclosures.json')))
 
 NEWS_ITEMS = NEWS_JSON['newsItems'].sort_by.with_index { |v, i| [Date.parse(v['date']), i] }.reverse
+SELF_DISCLOSURES_ITEMS = SELF_DISCLOSURES_JSON['newsItems'].sort_by.with_index { |v, i| [Date.parse(v['date']), i] }.reverse
