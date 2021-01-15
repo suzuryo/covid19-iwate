9af341c838058c07baaa0ce4739d522a7310f808
suzuryo <suzuryo@gmail.com>
2021-01-16 01:27:59 +0900

    WhatsNewCard の url.ja が存在しない場合

--- spec/lib/WhatsNewCard.rb
+++ spec/lib/WhatsNewCard.rb
@@ -7,7 +7,12 @@ def has_whats_new_card
   # テーブルの中身
   NEWS_ITEMS.each_with_index do |d, index|
     # テーブルの上からi行目をチェックする(icon + text)
-    expect(find("#WhatsNewCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1)").text).to eq "#{d['icon']} #{d['text']['ja']}"
+    if d['url']['ja'].blank?
+      expect(page).not_to have_selector("#WhatsNewCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1) > a")
+      expect(find("#WhatsNewCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1)").text).to eq "#{d['text']['ja']}"
+    else
+      expect(find("#WhatsNewCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(1) > a").text).to eq "#{d['icon']} #{d['text']['ja']}"
+    end
 
     # テーブルの上からi行目をチェックする(日付)
     expect(find("#WhatsNewCard > div > div > div.DataView-Content > div > div > div > table > tbody > tr:nth-child(#{1+index}) > td:nth-child(2)").text).to eq "#{Date.parse(d['date']).strftime("%-m月%-d日")}"
