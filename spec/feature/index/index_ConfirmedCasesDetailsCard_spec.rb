b060ab08b5bad9b9f7895bfc6bf7d2b4bcfc48f7
suzuryo <suzuryo@gmail.com>
2021-01-15 23:58:12 +0900

    ConfirmedCasesDetailsCard

--- spec/feature/index/index_ConfirmedCasesDetailsCard_spec.rb
+++ spec/feature/index/index_ConfirmedCasesDetailsCard_spec.rb
@@ -1,6 +1,5 @@
 require 'spec_helper'
-
-data_json = JSON.parse(File.read(File.join(__dir__, '../../../data/data.json')))
+require_relative '../../lib/ConfirmedCasesDetailsCard'
 
 describe "iPhone 6/7/8", type: :feature do
   context 'page [/]' do
@@ -13,41 +12,7 @@ describe "iPhone 6/7/8", type: :feature do
 
     describe '検査陽性者の状況(ConfirmedCasesDetailsCard)' do
       it '項目の値' do
-        # h3
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Header > div > div > h3').text).to eq '検査陽性者の状況'
-
-        # 陽性者数（累計）
-        d = number_to_delimited(data_json['patients']['data'].last['id'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-        # 入院
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][0]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(1) > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-        # 重症
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][0]['children'][1]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(1) > ul > li > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-        # 宿泊療養
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][1]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(2) > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-
-        # 自宅療養
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][2]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(3) > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-        # 入院・療養等調整中
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][3]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(4) > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-        # 退院等
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][5]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(5) > div > span:nth-child(2) > strong').text).to eq "#{d}"
-
-        # 死亡
-        d = number_to_delimited(data_json['main_summary']['children'][0]['children'][4]['value'])
-        expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(6) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+        confirmed_cases_details_card
 
         # index の card 一覧では breadcrumbs は表示されない
         expect(page).not_to have_selector('#ConfirmedCasesDetailsCard nav ul.v-breadcrumbs')
---
+++ spec/lib/ConfirmedCasesDetailsCard.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+def confirmed_cases_details_card
+  # h3
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Header > div > div > h3').text).to eq '検査陽性者の状況'
+
+  # 陽性者数（累計）
+  d = number_to_delimited(DATA_JSON['patients']['data'].last['id'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+  # 入院
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][0]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(1) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+  # 重症
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][0]['children'][1]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(1) > ul > li > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+  # 宿泊療養
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][1]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(2) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+
+  # 自宅療養
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][2]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(3) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+  # 入院・療養等調整中
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][3]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(4) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+  # 退院等
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][5]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(5) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+
+  # 死亡
+  d = number_to_delimited(DATA_JSON['main_summary']['children'][0]['children'][4]['value'])
+  expect(find('#ConfirmedCasesDetailsCard > div > div > div.DataView-Content > ul > li > ul > li:nth-child(6) > div > span:nth-child(2) > strong').text).to eq "#{d}"
+end
