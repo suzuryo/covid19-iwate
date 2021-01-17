3e3f771b361f0ccd8f25b88fb6dc22f10fdb9fed
suzuryo <suzuryo@gmail.com>
2021-01-17 14:32:37 +0900

    update index_spec

--- spec/feature/index/index_spec.rb
+++ spec/feature/index/index_spec.rb
@@ -1,7 +1,5 @@
 require 'spec_helper'
 
-ja_json = JSON.parse(File.read(File.join(__dir__, '../../../assets/locales/ja.json')))
-
 describe "iPhone 6/7/8", type: :feature do
   context 'page [/]' do
 
@@ -11,8 +9,8 @@ describe "iPhone 6/7/8", type: :feature do
     end
 
     it 'h1/h2' do
-      expect(find('#app > div > div.appContainer > div > div > header > h1').text).to eq "#{ja_json['Common']['新型コロナウイルス感染症']}\n#{ja_json['Common']['対策サイト']}"
-      expect(find('#app > div > div.appContainer > main > div > div > div.MainPage > div.Header.mb-3 > div.header > h2').text).to eq "#{ja_json['Common']['岩手の最新感染動向']}"
+      expect(find('#app > div > div.appContainer > div > div > header > h1').text).to eq "#{JA_JSON['Common']['新型コロナウイルス感染症']}\n#{JA_JSON['Common']['対策サイト']}"
+      expect(find('#app > div > div.appContainer > main > div > div > div.MainPage > div.Header.mb-3 > div.header > h2').text).to eq "#{JA_JSON['Common']['岩手の最新感染動向']}"
     end
 
     it '/flow' do
