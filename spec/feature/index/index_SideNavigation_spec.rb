194fea6d0ddef596090447779551a6d2e5d8207e
suzuryo <suzuryo@gmail.com>
2021-01-31 13:33:58 +0900

    rspec: SideNavigation

--- spec/feature/index/index_SideNavigation_spec.rb
+++ spec/feature/index/index_SideNavigation_spec.rb
@@ -6,12 +6,7 @@ describe 'iPhone 6/7/8', type: :feature do
   context 'page [/]' do
     before do
       visit '/'
-      render_lazy_contents
-    end
-
-    it 'h1/h2' do
-      expect(find('#app > div > div.appContainer > div > div > header > h1').text).to eq "#{JA_JSON['Common']['新型コロナウイルス感染症']}\n#{JA_JSON['Common']['対策サイト']}"
-      expect(find('#app > div > div.appContainer > main > div > div > div.MainPage > div.Header.mb-3 > div.header > h2').text).to eq (JA_JSON['Common']['岩手の最新感染動向']).to_s
+      # render_lazy_contents
     end
 
     it '/flow' do
