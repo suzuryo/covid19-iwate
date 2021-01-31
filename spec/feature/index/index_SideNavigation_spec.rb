f8b3d09d1c1ffb4ba4151e4185347279eb7505a7
suzuryo <suzuryo@gmail.com>
2021-01-31 13:52:18 +0900

    rspec: SideNavigation ja/en

--- spec/feature/index/index_SideNavigation_spec.rb
+++ spec/feature/index/index_SideNavigation_spec.rb
@@ -3,28 +3,30 @@
 require 'spec_helper'
 
 describe 'iPhone 6/7/8', type: :feature do
-  context 'page [/]' do
-    before do
-      visit '/'
-      # render_lazy_contents
-    end
+  LOCALES.each do |lang, data|
+    context "page #{data[:path]}" do
+      before do
+        visit data[:path]
+        render_lazy_contents
+      end
 
-    it '/flow' do
-      find('header.SideNavigation-Header > .SideNavigation-OpenIcon').click
-      find('ul.MenuList > li:nth-child(2) > a').click
-      expect(find('h2.pageTitle').text).to eq '新型コロナウイルス感染症が心配なときに'
-    end
+      it '/flow' do
+        find('header.SideNavigation-Header > .SideNavigation-OpenIcon').click
+        find('ul.MenuList > li:nth-child(2) > a').click
+        expect(find('h2.pageTitle').text).to eq data[:json]['SideNavigation']['a'][4].to_s
+      end
 
-    it '/work' do
-      find('header.SideNavigation-Header > .SideNavigation-OpenIcon').click
-      find('ul.MenuList > li:nth-child(4) > a').click
-      expect(find('h2.pageTitle').text).to eq '企業の皆様・はたらく皆様へ'
-    end
+      it '/work' do
+        find('header.SideNavigation-Header > .SideNavigation-OpenIcon').click
+        find('ul.MenuList > li:nth-child(4) > a').click
+        expect(find('h2.pageTitle').text).to eq data[:json]['SideNavigation']['a'][6].to_s
+      end
 
-    it '/about' do
-      find('header.SideNavigation-Header > .SideNavigation-OpenIcon').click
-      find('ul.MenuList > li:nth-child(7) > a').click
-      expect(find('h2.pageTitle').text).to eq '当サイトについて'
+      it '/about' do
+        find('header.SideNavigation-Header > .SideNavigation-OpenIcon').click
+        find('ul.MenuList > li:nth-child(7) > a').click
+        expect(find('h2.pageTitle').text).to eq data[:json]['Common']['当サイトについて'].to_s
+      end
     end
   end
 end
