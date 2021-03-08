21d2f0ed64e5ab5ec6a377ffe7889b78e35c26fc
suzuryo <suzuryo@gmail.com>
2021-03-08 15:58:46 +0900

    相談受付件数に更新停止のお知らせを追加

--- spec/lib/MonitoringConsultationDeskReportsNumberCard.rb
+++ spec/lib/MonitoringConsultationDeskReportsNumberCard.rb
@@ -38,4 +38,34 @@ def has_monitoring_consultation_desk_reports_number_card
   expect(page).to have_selector('#MonitoringConsultationDeskReportsNumberCard .DataViewExpansionPanel .v-expansion-panel--active')
   find('#MonitoringConsultationDeskReportsNumberCard .DataViewExpansionPanel button.v-expansion-panel-header').click
   expect(page).not_to have_selector('#MonitoringConsultationDeskReportsNumberCard .DataViewExpansionPanel .v-expansion-panel--active')
+
+  # 注釈を表示ボタンの文言
+  expect(find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel button.v-expansion-panel-header').text).to eq '注釈'
+
+  # 注釈を表示ボタンをクリックすると開く
+  expect(page).not_to have_selector('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content')
+  sleep 0.5
+  find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel button.v-expansion-panel-header').click
+  expect(page).to have_selector('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content')
+
+  # 注釈の中身をチェック
+  expect(find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content ul > li:nth-child(1)').text).to eq '2021年2月4日を最後に岩手県がこのデータの公表を止めたため、このグラフの更新が止まっている'
+  expect(find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content ul > li:nth-child(2)').text).to eq '主に「症状がある方からの受診等に関する相談」を計上'
+  expect(find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content ul > li:nth-child(3)').text).to eq 'コールセンター・各保健所等への受付件数の合計'
+  expect(find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content ul > li:nth-child(4)').text).to eq '窓口を開設した2020年2月8日からのデータを表示'
+  expect(find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content ul > li:nth-child(5)').text).to eq '岩手県の発表漏れによりデータの存在しない日がある'
+
+  # 注釈を表示ボタンをクリックすると閉じる
+  expect(page).to have_selector('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content')
+  find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel button.v-expansion-panel-header').click
+  expect(page).not_to have_selector('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel-content')
+
+  # データを表示と注釈を表示の両方を開く
+  expect(page).not_to have_selector('#MonitoringConsultationDeskReportsNumberCard .DataViewExpansionPanel .v-expansion-panel--active')
+  expect(page).not_to have_selector('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel--active')
+  find('#MonitoringConsultationDeskReportsNumberCard .DataViewExpansionPanel button.v-expansion-panel-header').click
+  sleep 1
+  find('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel button.v-expansion-panel-header').click
+  expect(page).to have_selector('#MonitoringConsultationDeskReportsNumberCard .DataViewExpansionPanel .v-expansion-panel--active')
+  expect(page).to have_selector('#MonitoringConsultationDeskReportsNumberCard .NotesExpansionPanel .v-expansion-panel--active')
 end
