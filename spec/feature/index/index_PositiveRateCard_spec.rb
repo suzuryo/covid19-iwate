85d12684858d5c3cadf5461be39ace1988576e24
suzuryo <suzuryo@gmail.com>
2021-05-05 16:24:04 +0900

    spec: PositiveRate の locale 対応

--- spec/feature/index/index_PositiveRateCard_spec.rb
+++ spec/feature/index/index_PositiveRateCard_spec.rb
@@ -5,17 +5,19 @@ require_relative '../../lib/BreadCrumbs'
 require_relative '../../lib/PositiveRateCard'
 
 describe 'iPhone 6/7/8', type: :feature do
-  context 'page [/]' do
-    before do
-      visit '/'
-      render_lazy_contents
-      page.scroll_to('#PositiveRateCard')
-    end
+  LOCALES.each do |lang, data|
+    context "page #{data[:path]}" do
+      before do
+        visit data[:path]
+        render_lazy_contents
+        page.scroll_to('#PositiveRateCard')
+      end
 
-    describe '検査の陽性率(PositiveRateCard)' do
-      it '項目の値' do
-        has_positive_rate_card
-        has_no_breadcrumbs(id: '#PositiveRateCard')
+      describe '検査の陽性率(PositiveRateCard)' do
+        it '項目の値' do
+          has_positive_rate_card(lang: lang, lang_json: data[:json])
+          has_no_breadcrumbs(id: '#PositiveRateCard')
+        end
       end
     end
   end
